c79c2c401bc82dbb2e6baa2f7f9c94faa16ba4d4
Jussi Pakkanen <jpakkane@gmail.com>
2016-12-27 16:12:14 +0200

    Measure size with -fno-exceptions. Weirdly it is bigger. Tested also when throwing from last function but it did not matter.

--- measure.py
+++ measure.py
@@ -28,18 +28,34 @@ class Measurer:
         self.cpp_exe = os.path.join(self.build_cpp, 'cppbin')
         self.meson_exe = '../meson/meson.py'
 
-    def run(self):
-        if os.path.exists(self.build_cpp):
-            shutil.rmtree(self.build_cpp)
-        subprocess.check_call('./generate.py')
-        subprocess.check_call([self.meson_exe, '--buildtype=debugoptimized', self.source_cpp, self.build_cpp])
-        subprocess.check_call(['ninja', '-C', self.build_cpp])
-        cpp_size = fsize(self.cpp_exe)
-        subprocess.check_call(['strip', self.cpp_exe])
-        cpp_stripped_size = fsize(self.cpp_exe)
+    def build_and_measure(self, src_dir, build_dir, bin_name, extra_args = [], env=None):
+        if os.path.exists(build_dir):
+            shutil.rmtree(build_dir)
+        subprocess.check_call([self.meson_exe, src_dir, build_dir] + extra_args, env=env)
+        subprocess.check_call(['ninja', '-C', build_dir])
+        size = fsize(bin_name)
+        subprocess.check_call(['strip', bin_name])
+        stripped_size = fsize(bin_name)
+        return (size, stripped_size)
 
+    def run(self):
+        subprocess.check_call('./generate.py')
+
+        cpp_size, cpp_stripped_size = self.build_and_measure(self.source_cpp,
+                                                             self.build_cpp,
+                                                             self.cpp_exe,
+                                                             ['--buildtype=debugoptimized'])
+        noexc_env = os.environ.copy()
+        noexc_env['CXXFLAGS'] = '-fno-exceptions'
+        noexc_size, noexc_stripped_size = self.build_and_measure(self.source_cpp,
+                                                                 self.build_cpp,
+                                                                 self.cpp_exe,
+                                                                 ['--buildtype=debugoptimized'],
+                                                                 env=noexc_env)
         print('C++ unstripped', cpp_size)
         print('C++ stripped', cpp_stripped_size)
+        print('C++ (noexcept) unstripped', noexc_size)
+        print('C++ (noexcept) stripped', noexc_stripped_size)
 
 if __name__ == '__main__':
     m = Measurer()
